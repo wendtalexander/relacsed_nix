fafaf1717191f3cfaebe8f8d3fd975b912df031c
Jan Grewe <jan.grewe@g-node.org>
2021-10-17 20:18:51 +0200

    [repro] docstrings

--- rlxnix/repro.py
+++ rlxnix/repro.py
@@ -1,8 +1,16 @@
 import nixio
 
 class RePro(object):
+    """This class represents the data of a RePro run. It offers access to the data and metadata.
+    """
 
     def __init__(self, repro_run: nixio.Tag, relacs_nix_version=1.1):
+        """Create a RePro instance that represent one run of a relacs RePro.
+
+        Args:
+            repro_run (nixio.Tag): the nix - tag that belong to the repro run 
+            relacs_nix_version (float, optional): The mapping version number. Defaults to 1.1.
+        """
         super().__init__()
         self._repro_run = repro_run
         self._relacs_nix_version = relacs_nix_version
@@ -11,12 +19,17 @@ class RePro(object):
 
     @property
     def start_time(self):
+        """The start time of the 
+
+        Returns:
+            [type]: [description]
+        """
         return self._start_time
 
     @property
     def duration(self):
         return self._duration
-    
+
     @property
     def repro_tag(self):
         return self._repro_run

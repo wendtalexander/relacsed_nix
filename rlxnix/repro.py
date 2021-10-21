92ccedc2bc859f184eb156aeb1f6c39d3dc725f4
Jan Grewe <jan.grewe@g-node.org>
2021-10-21 11:10:37 +0200

    [repro/stimulus] inherit from trace container

--- rlxnix/repro.py
+++ rlxnix/repro.py
@@ -1,11 +1,11 @@
-from IPython.terminal.embed import embed
 import nixio
-import numpy as np
 
-from rlxnix.stimulus import Stimulus
+from .trace_container import TraceContainer
+from .stimulus import Stimulus
+from .util import nix_metadata_to_dict
 
 
-class ReProRun(object):
+class ReProRun(TraceContainer):
     """This class represents the data of a RePro run. It offers access to the data and metadata.
     """
 
@@ -16,106 +16,12 @@ class ReProRun(object):
             repro_run (nixio.Tag): the nix - tag that belong to the repro run 
             relacs_nix_version (float, optional): The mapping version number. Defaults to 1.1.
         """
-        super().__init__()
-        self._repro_run = repro_run
-        self._relacs_nix_version = relacs_nix_version
-        self._start_time = repro_run.position[0]
-        self._duration = repro_run.extent[0]
+        super().__init__(repro_run, relacs_nix_version=relacs_nix_version)
         self._stimuli = []
-
-    @property
-    def name(self) -> str:
-        """The name of the repro run
-
-        Returns:
-            string: the name
-        """
-        return self._repro_run.name
-
-    @property
-    def type(self) -> str:
-        """The type of the repro run
-
-        Returns:
-            string: the type
-        """
-        return self._repro_run.type
-
-    @property
-    def start_time(self) -> float:
-        """The start time of the 
-
-        Returns:
-            float: RePro start time
-        """
-        return self._start_time
-
-    @property
-    def duration(self) -> float:
-        """The duration of the repro run in seconds.
-
-        Returns:
-            float: the duration in seconds.
-        """
-        return self._duration
-
-    @property
-    def repro_tag(self) -> nixio.Tag:
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
-        return self._repro_run
-
-    @property
-    def references(self) -> list:
-        """The list of referenced event and data traces
-
-        Returns:
-            List: index, name and type of the references
-        """
-        refs = []
-        for i, r in enumerate(self._repro_run.references):
-            refs.append((i, r.name, r.type))
-        return refs
-
-    @property
-    def features(self) -> list:
-        """List of features associated with this repro run.
-
-        Returns:
-            List: name and type of t[description]
-        """
-        features = []
-        for i, feats in enumerate(self._repro_run.features):
-            features.append((i, feats.data.name, feats.data.type))
-        return features
-
-    def trace_data(self, name_or_index):
-        """Get the data that was recorded while this repro was run.
-
-        Args:
-            name_or_index (str or int): name or index of the referenced data trace
-
-        Returns:
-            data (numpy array): the data 
-            time (numpy array): the respective time vector, None, if the data is an event trace
-        """
-        ref = self._repro_run.references[name_or_index]
-        time = None
-        data = ref.get_slice([self.start_time], [self.duration], nixio.DataSliceMode.Data)[:]
-        if "relacs.data.sampled" in ref.type:
-            time = np.array(ref.dimensions[0].axis(len(data), start_position=self.start_time))
-        return data, time
-
-    def feature_data(self, name_or_index):
-        feat_data = self._repro_run.feature_data(name_or_index)
-        return feat_data[:]
     
     @property
     def metadata(self):
-        m = util.nix_metadata_to_dict(self._repro_run.metadata)
+        m = nix_metadata_to_dict(self._repro_run.metadata)
         return m
 
     def add_stimulus(self, stimulus:Stimulus):
--- rlxnix/stimulus.py
+++ rlxnix/stimulus.py
@@ -1,68 +1,17 @@
-import numpy as np
 import nixio
 
-from IPython import embed
+from .trace_container import TraceContainer
 
 
-class Stimulus(object):
+class Stimulus(TraceContainer):
+    """[summary]
 
-    def __init__(self, stimulus_mtag: nixio.MultiTag, index: int) -> None:
-        super().__init__()
+    Args:
+        TraceContainer ([type]): [description]
+    """
+    def __init__(self, stimulus_mtag: nixio.MultiTag, index: int, relacs_nix_version=1.1) -> None:
+        super().__init__(stimulus_mtag, index, relacs_nix_version=relacs_nix_version)
         self._mtag = stimulus_mtag
-        self._index = index
-
-    @property
-    def start_time(self):
-        return self._mtag.positions[self._index, 0][0]
-
-    @property
-    def duration(self):
-        return self._mtag.extents[self._index, 0][0] if self._mtag.extents else 0.0
-
-    @property
-    def references(self) -> list:
-        """The list of referenced event and data traces
-
-        Returns:
-            List: index, name and type of the references
-        """
-        refs = []
-        for i, r in enumerate(self._mtag.references):
-            refs.append((i, r.name, r.type))
-        return refs
-
-    @property
-    def features(self) -> list:
-        """List of features associated with this repro run.
-
-        Returns:
-            List: name and type of t[description]
-        """
-        features = []
-        for i, feats in enumerate(self._mtag.features):
-            features.append((i, feats.data.name, feats.data.type))
-        return features
-
-    def trace_data(self, name_or_index):
-        """Get the data that was recorded while this repro was run.
-
-        Args:
-            name_or_index (str or int): name or index of the referenced data trace
-
-        Returns:
-            data (numpy array): the data 
-            time (numpy array): the respective time vector, None, if the data is an event trace
-        """
-        ref = self._mtag.references[name_or_index]
-        time = None
-        data = ref.get_slice([self.start_time], [self.duration], nixio.DataSliceMode.Data)[:]
-        if "relacs.data.sampled" in ref.type:
-            time = np.array(ref.dimensions[0].axis(len(data), start_position=self.start_time))
-        return data, time
-    
-    def feature_data(self, name_or_index):
-        feat_data = self._mtag.feature_data(name_or_index, self._index)
-        return feat_data[:]
 
     def __str__(self) -> str:
         name = self._mtag.name

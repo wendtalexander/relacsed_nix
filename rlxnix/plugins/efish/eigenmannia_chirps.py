40a972c6f3a74747c228af046a50980c3378979b
Jan Grewe <jan.grewe@g-node.org>
2021-10-27 11:19:33 +0200

    [eigen chirps] docstrings, and a new overview plot

--- rlxnix/plugins/efish/eigenmannia_chirps.py
+++ rlxnix/plugins/efish/eigenmannia_chirps.py
@@ -1,7 +1,9 @@
 import nixio
-from ...repro import ReProRun
+import numpy as np
+import matplotlib.pyplot as plt
 
-from IPython.terminal.embed import embed
+from ...repro import ReProRun
+from ...mappings import DataType
 
 
 class EigenmanniaChirps(ReProRun):
@@ -14,12 +16,29 @@ class EigenmanniaChirps(ReProRun):
         if stimulus_index >= len(self.stimuli) or stimulus_index < 0:
             raise IndexError(f"Stimulus index {stimulus_index} is out of bounds for number of stimuli {len(self.stimuli)}")
 
-    def _check_trace(self, trace_name):
+    def _check_trace(self, trace_name, data_type=DataType.Continuous):
         if trace_name not in self._tag.references:
             raise ValueError(f"Trace {trace_name} not found!")
+        trace = self._trace_map[trace_name]
+        if trace.trace_type != data_type:
+            raise ValueError(f"Data type of trace {trace.name} does not match expected data type (expected: {data_type}, found: {trace.trace_type}).")        
 
     def spikes(self, stimulus_index=None, trace_name="Spikes-1"):
-        self._check_trace(trace_name)
+        """Return the spike times for the whole repro run or during a certain stimulus presentation
+
+        Parameters
+        ----------
+        stimulus_index : int, optional
+            The stimulus index. If None, the spikes of the whole repro run will be read from file. By default None.
+        trace_name : str, optional
+            The name of the spikes event trace, by default "Spikes-1"
+
+        Returns
+        -------
+        numpy ndarray
+            The spike times in seconds. Times are relative to stimulus onset.
+        """
+        self._check_trace(trace_name, DataType.Event)
         if stimulus_index is not None:
             self._check_stimulus(stimulus_index)
             return self.stimuli[stimulus_index].trace_data(trace_name)[0]
@@ -27,39 +46,183 @@ class EigenmanniaChirps(ReProRun):
             return self.trace_data(trace_name)[0]
 
     def local_eod(self, stimulus_index=None, trace_name="LocalEOD-1"):
-        self._check_trace(trace_name)
+        """Return the local eod measurement for the whole repro run or during a certain stimulus presentation.
+
+        Parameters
+        ----------
+        stimulus_index :  int, optional
+            The stimulus index. If None, the spikes of the whole repro run will be read from file. By default None.
+        trace_name : str, optional
+            The name of the spikes event trace, by default "LocalEOD-1"
+
+        Returns
+        -------
+        np.ndarray 
+            the local eod data
+        np.ndarray
+            the respective time axis
+        """
+        self._check_trace(trace_name, data_type=DataType.Continuous)
         
         if stimulus_index is not None:
             self._check_stimulus(stimulus_index)
             return self.stimuli[stimulus_index].trace_data(trace_name)
         else:
             return self.trace_data(trace_name)
-    
-    def chirp_times(self, stimulus_index=0):
-        self._check_stimulus(stimulus_index)
-        stimulus = self.stimuli[stimulus_index]
-        metadata = stimulus.metadata
-        cts = metadata[stimulus.name]["ChirpTimes"][0]
-        return cts
+
+    def membrane_voltage(self, stimulus_index=None, trace_name="V-1"):
+        """Returns the membrane potential measurement for the whole repro run or during a certain stimulus presentation.
+
+        Parameters
+        ----------
+        stimulus_index : int, optional
+            The stimulus index. If None, the voltage trace of the whole repro run will be read from file. By default None.
+        trace_name : str, optional
+            The name of the membrane voltage trace, by default "V-1"
+
+        Returns
+        -------
+        np.ndarray 
+            the membrane potential
+        np.ndarray
+            the respective time axis
+        """
+        self._check_trace(trace_name, data_type=DataType.Continuous)
+        
+        if stimulus_index is not None:
+            self._check_stimulus(stimulus_index)
+            return self.stimuli[stimulus_index].trace_data(trace_name)
+        else:
+            return self.trace_data(trace_name)
+
+    def stimulus_output(self, stimulus_index=None, trace_name="GlobalEFieldStimulus"):
+        """Returns the recorded stimulus trace for the whole repro run or during a certain stimulus presentation.
+
+        Parameters
+        ----------
+        stimulus_index : int, optional
+            The stimulus index. If None, the stimulus trace of the whole repro run will be read from file. By default None.
+        trace_name : str, optional
+            The name of the recorded trace, by default "GlobalEFieldStimulus"
+
+        Returns
+        -------
+        np.ndarray 
+            the stimulus trace
+        np.ndarray
+            the respective time axis
+        """
+        self._check_trace(trace_name, data_type=DataType.Continuous)
+        
+        if stimulus_index is not None:
+            self._check_stimulus(stimulus_index)
+            return self.stimuli[stimulus_index].trace_data(trace_name)
+        else:
+            return self.trace_data(trace_name)
+
+    @property
+    def chirp_times(self):
+        """ The times of the artificial chirps of a given stimulus presentation.
+
+        Returns
+        -------
+        list
+            The chirp times relative to stimulus onset
+        str
+            The unit
+        """
+        cts = []
+        unit = ""
+        for s in self.stimuli:
+            metadata = s.metadata
+            cts.append(metadata[s.name]["ChirpTimes"][0])
+            unit = s.metadata[s.name]["ChirpTimes"][1]
+        return cts, unit
+
 
     @property
     def chirp_type(self):
-        return self.metadata["RePro-Info"]["settings"]["chirptype"]
+        """The type of the simulated chirp, e.g. Type A or Type B
+
+        Returns
+        -------
+        str
+            The type
+        """
+        return self.metadata["RePro-Info"]["settings"]["chirptype"][0][0]
 
     @property
     def delta_f(self):
+        """The difference frequency to the recorded fish's EOD frequency for all stimulus presentations
+
+        Returns
+        -------
+        list
+            List containing the dfs
+        str
+            the unit
+        """
         dfs = []
         unit = ""
         for s in self.stimuli:
-            dfs.append(s.metadata[s.name]["deltaf"][0])
+            dfs.append(s.metadata[s.name]["deltaf"][0][0])
             unit = s.metadata[s.name]["deltaf"][1]
         return dfs, unit
 
     @property
     def chirp_duration(self):
+        """The chirp durations of the stimulus presentations.
+
+        Returns
+        -------
+        list
+            List containing the chirp durations
+        str
+            the unit
+        """
         cds = []
         unit = ""
         for s in self.stimuli:
-            cds.append(s.metadata[s.name]["chirp duration"][0])
+            cds.append(s.metadata[s.name]["chirp duration"][0][0])
             unit = s.metadata[s.name]["chirp duration"][1]
         return cds, unit
+
+    def _plot_axis(self, axis, x_data, y_data, spikes, chirp_times, ylabel):
+        axis.plot(x_data, y_data, lw=0.5, color="tab:blue", label="voltage")
+        axis.scatter(spikes, np.ones_like(spikes) * np.max(y_data), s=10, marker="*", c="tab:green", label="spikes")
+        axis.scatter(chirp_times, np.ones_like(chirp_times) * np.min(y_data), s=20, marker="o", c="tab:red", label="chirps")
+        axis.set_ylabel(ylabel)
+        axis.spines["top"].set_visible(False)
+        axis.spines["right"].set_visible(False)
+        axis.set_xlim([x_data[0], x_data[-1]])
+
+    def plot_overview(self, stimulus_index=0, filename=None):
+        """[summary]
+
+        Parameters
+        ----------
+        stimulus_index : int, optional
+            The stimulus index, by default 0
+        filename: str, optional
+            The filename for the figure. If not given, the plot will be shown. By default None
+        
+        """
+        spikes = self.spikes(stimulus_index=stimulus_index)
+        voltage, time = self.membrane_voltage(stimulus_index=stimulus_index)
+        eod, eod_time = self.local_eod(stimulus_index=stimulus_index)
+        stim, stim_time = self.stimulus_output(stimulus_index=stimulus_index)
+        chirp_times, _ = self.chirp_times
+        c_times = chirp_times[stimulus_index]
+
+        fig, axes = plt.subplots(ncols=1, nrows=3)
+        self._plot_axis(axes[0], time, voltage, spikes, c_times, "voltage [mV]")
+        axes[0].legend(fontsize=7, ncol=3, loc=(0.5, 1.05))
+        self._plot_axis(axes[1], eod_time, eod, spikes, c_times, "voltage [mV]")
+        self._plot_axis(axes[2], stim_time, stim, spikes, c_times, "voltage [mV]")
+        axes[-1].set_xlabel("time [s]")
+        
+        if filename is not None:
+            fig.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
--- test.py
+++ test.py
@@ -15,22 +15,29 @@ if __name__ == "__main__":
         d = rlx.Dataset("/media/grewe/pocketbrain/data/2021-09-03-bv-invivo-2/2021-09-03-bv-invivo-2.nix")
 
     sam_runs = d.repro_runs("sam", False)
-    baseline_data = d.repro_runs("baseline", False)[0]
-    
-    sam = sam_runs[0]
-    sam_stim0 = sam.stimuli[0]
-    print(sam_stim0.id)
-    print(sam_stim0.repro_tag_id)
-    data, time = sam_stim0.trace_data("LocalEOD-1")
-    sam_spikes, _ = sam_stim0.trace_data("Spikes-1")
-    
-    plt.plot(time, data)
-    plt.scatter(sam_spikes, np.ones_like(sam_spikes)*np.max(data), color="tab:red")
+    if len(sam_runs) > 0: 
+        sam = sam_runs[0]
+        sam_stim0 = sam.stimuli[0]
+        print(sam_stim0.id)
+        print(sam_stim0.repro_tag_id)
+        data, time = sam_stim0.trace_data("LocalEOD-1")
+        sam_spikes, _ = sam_stim0.trace_data("Spikes-1")
+        
+        plt.plot(time, data)
+        plt.scatter(sam_spikes, np.ones_like(sam_spikes)*np.max(data), color="tab:red")
 
-
-    bd, bt = baseline_data.trace_data("V-1")
-    spike_times, _ = baseline_data.trace_data("Spikes-1")
-    plt.plot(bt, bd)
-    plt.scatter(spike_times, np.ones_like(spike_times) * np.max(bd))
+    baseline_data = d.repro_runs("baseline", False)
+    if len(baseline_data) > 0:
+        bd, bt = baseline_data[0].trace_data("V-1")
+        spike_times, _ = baseline_data[0].trace_data("Spikes-1")
+        plt.plot(bt, bd)
+        plt.scatter(spike_times, np.ones_like(spike_times) * np.max(bd))
     plt.show()
+
+
+    chirps = d.repro_runs("Eigen", exact=False)
+    if len(chirps) > 0:
+        chirp = chirps[0]
+        chirp.plot_overview()
+    
     embed()

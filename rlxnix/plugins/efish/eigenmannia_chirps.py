103da8b236180f15fe987ff0cb409ca66de4c8d5
Jan Grewe <jan.grewe@g-node.org>
2021-10-28 11:28:14 +0200

    [eigenmannia] inherits EfishEphys superclass

--- rlxnix/plugins/efish/eigenmannia_chirps.py
+++ rlxnix/plugins/efish/eigenmannia_chirps.py
@@ -2,123 +2,14 @@ import nixio
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ...repro import ReProRun
-from ...mappings import DataType
+from .efish_ephys_repro import EfishEphys
 
 
-class EigenmanniaChirps(ReProRun):
+class EigenmanniaChirps(EfishEphys):
     _repro_name = "EigenmanniaChirps"
 
     def __init__(self, repro_run: nixio.Tag, traces, relacs_nix_version=1.1):
         super().__init__(repro_run, traces, relacs_nix_version=relacs_nix_version)
-    
-    def _check_stimulus(self, stimulus_index):
-        if stimulus_index >= len(self.stimuli) or stimulus_index < 0:
-            raise IndexError(f"Stimulus index {stimulus_index} is out of bounds for number of stimuli {len(self.stimuli)}")
-
-    def _check_trace(self, trace_name, data_type=DataType.Continuous):
-        if trace_name not in self._tag.references:
-            raise ValueError(f"Trace {trace_name} not found!")
-        trace = self._trace_map[trace_name]
-        if trace.trace_type != data_type:
-            raise ValueError(f"Data type of trace {trace.name} does not match expected data type (expected: {data_type}, found: {trace.trace_type}).")        
-
-    def spikes(self, stimulus_index=None, trace_name="Spikes-1"):
-        """Return the spike times for the whole repro run or during a certain stimulus presentation
-
-        Parameters
-        ----------
-        stimulus_index : int, optional
-            The stimulus index. If None, the spikes of the whole repro run will be read from file. By default None.
-        trace_name : str, optional
-            The name of the spikes event trace, by default "Spikes-1"
-
-        Returns
-        -------
-        numpy ndarray
-            The spike times in seconds. Times are relative to stimulus onset.
-        """
-        self._check_trace(trace_name, DataType.Event)
-        if stimulus_index is not None:
-            self._check_stimulus(stimulus_index)
-            return self.stimuli[stimulus_index].trace_data(trace_name)[0]
-        else:
-            return self.trace_data(trace_name)[0]
-
-    def local_eod(self, stimulus_index=None, trace_name="LocalEOD-1"):
-        """Return the local eod measurement for the whole repro run or during a certain stimulus presentation.
-
-        Parameters
-        ----------
-        stimulus_index :  int, optional
-            The stimulus index. If None, the spikes of the whole repro run will be read from file. By default None.
-        trace_name : str, optional
-            The name of the spikes event trace, by default "LocalEOD-1"
-
-        Returns
-        -------
-        np.ndarray 
-            the local eod data
-        np.ndarray
-            the respective time axis
-        """
-        self._check_trace(trace_name, data_type=DataType.Continuous)
-        
-        if stimulus_index is not None:
-            self._check_stimulus(stimulus_index)
-            return self.stimuli[stimulus_index].trace_data(trace_name)
-        else:
-            return self.trace_data(trace_name)
-
-    def membrane_voltage(self, stimulus_index=None, trace_name="V-1"):
-        """Returns the membrane potential measurement for the whole repro run or during a certain stimulus presentation.
-
-        Parameters
-        ----------
-        stimulus_index : int, optional
-            The stimulus index. If None, the voltage trace of the whole repro run will be read from file. By default None.
-        trace_name : str, optional
-            The name of the membrane voltage trace, by default "V-1"
-
-        Returns
-        -------
-        np.ndarray 
-            the membrane potential
-        np.ndarray
-            the respective time axis
-        """
-        self._check_trace(trace_name, data_type=DataType.Continuous)
-        
-        if stimulus_index is not None:
-            self._check_stimulus(stimulus_index)
-            return self.stimuli[stimulus_index].trace_data(trace_name)
-        else:
-            return self.trace_data(trace_name)
-
-    def stimulus_output(self, stimulus_index=None, trace_name="GlobalEFieldStimulus"):
-        """Returns the recorded stimulus trace for the whole repro run or during a certain stimulus presentation.
-
-        Parameters
-        ----------
-        stimulus_index : int, optional
-            The stimulus index. If None, the stimulus trace of the whole repro run will be read from file. By default None.
-        trace_name : str, optional
-            The name of the recorded trace, by default "GlobalEFieldStimulus"
-
-        Returns
-        -------
-        np.ndarray 
-            the stimulus trace
-        np.ndarray
-            the respective time axis
-        """
-        self._check_trace(trace_name, data_type=DataType.Continuous)
-        
-        if stimulus_index is not None:
-            self._check_stimulus(stimulus_index)
-            return self.stimuli[stimulus_index].trace_data(trace_name)
-        else:
-            return self.trace_data(trace_name)
 
     @property
     def chirp_times(self):
@@ -205,7 +96,7 @@ class EigenmanniaChirps(ReProRun):
             The stimulus index, by default 0
         filename: str, optional
             The filename for the figure. If not given, the plot will be shown. By default None
-        
+
         """
         spikes = self.spikes(stimulus_index=stimulus_index)
         voltage, time = self.membrane_voltage(stimulus_index=stimulus_index)
@@ -220,7 +111,7 @@ class EigenmanniaChirps(ReProRun):
         self._plot_axis(axes[1], eod_time, eod, spikes, c_times, "voltage [mV]")
         self._plot_axis(axes[2], stim_time, stim, spikes, c_times, "voltage [mV]")
         axes[-1].set_xlabel("time [s]")
-        
+
         if filename is not None:
             fig.savefig(filename)
             plt.close()

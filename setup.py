fcbe8b40e6f5a7a4b97c951fd589d31e71440500
Jan Grewe <jan.grewe@g-node.org>
2021-11-05 14:45:11 +0100

    [setup] set package classifiers

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ NAME = "rlxnix"
 VERSION = 0.5
 AUTHOR = "Jan Grewe"
 CONTACT = "jan.grewe@g-node.org"
-CLASSIFIERS = "science"
 DESCRIPTION = "Reader for relacs written nix files"
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering'
+]
 
 README = "README.md"
 with open(README) as f:
@@ -18,11 +24,11 @@ setup(
     description=DESCRIPTION,
     author=AUTHOR,
     author_email=CONTACT,
+    classifiers=classifiers,
     packages=find_packages(),
     install_requires=install_req,
     include_package_data=True,
     long_description=description_text,
     long_description_content_type="text/markdown",
-    classifiers=CLASSIFIERS,
     license="BSD"
 )

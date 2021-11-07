f3efe16cd31e0fbb5ef8a386e2bd56ef9eab4b8d
Jan Grewe <jan.grewe@g-node.org>
2021-11-07 01:05:43 +0100

    [info, init] some fixes, limit * imports

--- rlxnix/__init__.py
+++ rlxnix/__init__.py
@@ -3,3 +3,10 @@ from .base.trace_container import TimeReference
 from .utils.timeline import IntervalMode
 from .utils.util import data_links_to_pandas
 from .utils.data_loader import load_data_segment
+
+from .info import VERSION, AUTHOR
+
+__version__ = VERSION
+__author__ = AUTHOR
+__all__ = ["Dataset", "TimeReference", "IntervalMode", 
+           "data_links_to_pandas", "load_data_segment"]

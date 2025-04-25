8d32a55e1a47ff51d1a82144ae828e5139eed912
wendtalexander <wendtalexander@protonmail.com>
2025-04-25 10:14:08 +0200

    [mappings] adding open-ephys mappings

--- rlxnix/utils/mappings.py
+++ rlxnix/utils/mappings.py
@@ -8,13 +8,14 @@ class DataType(Enum):
       Event = 1
       StimulusSegment = 2
 
-
 type_map = {1.0: {DataType.Event: "nix.events.position",
                   DataType.Continuous: "nix.data.sampled",
                   DataType.StimulusSegment: "nix.event.stimulus"},
-            1.1: {DataType.Event: "relacs.data.event",
-                  DataType.Continuous: "relacs.data.sampled",
-                  DataType.StimulusSegment: "relacs.stimulus"}
+
+            1.1:
+                 {DataType.Event: ["relacs.data.events" ,"open-ephys.data.events"],
+                  DataType.Continuous: ["relacs.data.sampled","open-ephys.data.sampled"],
+                  DataType.StimulusSegment: ["relacs.stimulus", "open-ephys.stimulus"]},
             }
 
 
